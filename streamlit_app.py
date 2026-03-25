cc49c424572d5f0c96aa8ce343ab4e04673dca76
Kevin Ebuen <36286544+kcebuen@users.noreply.github.com>
2026-03-25 17:14:25 +0800

    Import requests and fetch fruit nutrition data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-
+import requests  
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -34,6 +34,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
@@ -49,7 +52,4 @@ if ingredients_list:
 
         st.success('Your Smoothie is ordered, ' + name_on_order+ '!', icon="✅")
 
-import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
